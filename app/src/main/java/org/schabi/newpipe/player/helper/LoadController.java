bb5188c8ead1c40d49c9e8a13186bc9e276bbc93
AmmarDeeper34 <187941461+AmmarDeeper34@users.noreply.github.com>
2026-01-16 09:47:42 +0000

    feat: implement Legacy Engine for SD435 (50s buffer, 720p cap, OOM fixes)

--- app/src/main/java/org/schabi/newpipe/player/helper/LoadController.java
+++ app/src/main/java/org/schabi/newpipe/player/helper/LoadController.java
@@ -1,11 +1,21 @@
 package org.schabi.newpipe.player.helper;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.upstream.DefaultAllocator;
 
 public class LoadController extends DefaultLoadControl {
 
     public static final String TAG = "LoadController";
     private boolean preloadingEnabled = true;
+
+    /**
+     * Legacy Engine: 50s buffer for Android 6 / Snapdragon 435 stability.
+     * @return a configured LoadControl instance
+     */
+    public static LoadControl getLoadControl() {
+        return new LoadController();
+    }
 
     @Override
     public void onPrepared() {
@@ -25,6 +35,13 @@ public class LoadController extends DefaultLoadControl {
         super.onReleased();
     }
 
+    /**
+     * Determines if loading should continue based on preloading state.
+     * @param playbackPositionUs the playback position in microseconds
+     * @param bufferedDurationUs the buffered duration in microseconds
+     * @param playbackSpeed the current playback speed
+     * @return whether loading should continue
+     */
     @Override
     public boolean shouldContinueLoading(final long playbackPositionUs,
                                          final long bufferedDurationUs,
@@ -35,6 +52,7 @@ public class LoadController extends DefaultLoadControl {
         return super.shouldContinueLoading(
                 playbackPositionUs, bufferedDurationUs, playbackSpeed);
     }
+
 
     public void disablePreloadingOfCurrentTrack() {
         preloadingEnabled = false;
